b393d702bc02003b9522878097ed835f28ce2e7c
Lavitr <lavitr@yandex.ru>
2020-10-19 09:58:44 +0300

    feat: implementation logger and error handler

--- src/app.js
+++ src/app.js
@@ -5,13 +5,29 @@ const YAML = require('yamljs');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
+const { middlewareInfo, middlewareError, logger } = require('./common/logger');
+const { errorHandler } = require('./common/errorHandlers.js');
+const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 const app = express();
-const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+
+process
+  .on('uncaughtException', err => {
+    logger.log('error', `${err.message}`);
+    // eslint-disable-next-line no-process-exit
+    logger.on('finish', () => process.exit(1));
+  })
+  .on('unhandledRejection', async reason => {
+    logger.log('error', `${reason.message}`);
+    // eslint-disable-next-line no-process-exit
+    logger.on('finish', () => process.exit(1));
+  });
 
 app.use(express.json());
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.use(middlewareInfo);
 
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
@@ -25,10 +41,25 @@ app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 boardRouter.use('/:boardId/tasks', taskRouter);
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-  next();
-});
+app.use(errorHandler);
+app.use(middlewareError);
+
+//* ******  for check 'uncaughtException' uncomment lines below
+
+// setInterval(() => {
+//   console.log('working');
+// }, 1000);
+// setInterval(() => {
+//   throw Error('Oops uncaughtException!');
+// }, 2500);
+
+//* ****** for check 'unhandledRejection' uncomment lines below
+
+// setInterval(() => {
+//   console.log('working');
+// }, 1000);
+// setInterval(() => {
+//   Promise.reject(new Error('Oops! unhandledRejection'));
+// }, 2500);
 
 module.exports = app;
